869feeebdb2e527eafdc2ce8f211595fe348d024
Caleb Francis <calebonesimus@gmail.com>
2015-07-13 14:03:46 -0400

    Create Shoe for blackjack
    Add Shoe class and edit blackjack.rb to utilize shoe for the deck

--- blackjack.rb
+++ blackjack.rb
@@ -1,26 +1,26 @@
 require_relative './deck'
 
 class Game
-	
+
 	def intro
 		# welcome to the game
 		puts "Welcome to blackjack!"
 		self.continue?
 	end
-	
+
 	def continue?
 		# prompt to continue or exit game
 		print "Deal new hand? (y/n) "
 		gets.downcase.chomp == "y" ? true : exit
-		self.play		
+		self.play
 	end
-	
+
 	def play
 		# initialize per game variables
 		@dealer_hand = []
 		@player_hand = []
 		@player_done = nil
-		@deck = Deck.new
+		@deck = Shoe.new
 		2.times { @dealer_hand << @deck.deal }
 		2.times { @player_hand << @deck.deal }
 		# send to results
@@ -35,11 +35,11 @@ class Game
 		elsif choice == "s"
 			@player_done = true
 			self.hit("dealer")
-		else	
+		else
 			self.hit_or_stand?
 		end
 	end
-	
+
 	def hit(person)
 		if person == "player"
 			# player hits and sends back to results to let hit again
@@ -75,7 +75,7 @@ class Game
 		puts "\nPLAYER:\n-------"
 		@player_hand.each do |card|
 			puts "#{card.face} of #{card.suit} (#{card.value})"
-			@player_total += card.value		
+			@player_total += card.value
 		end
 		puts "---> TOTAL (#{@player_total})"
 		print "\nYou have #{@player_total}. "
@@ -111,7 +111,7 @@ class Game
 		# prompt to start new game or end
 		self.continue?
 	end
-	
+
 end
 
 # start game
